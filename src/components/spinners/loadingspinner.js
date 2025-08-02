6dabf32e79958371e1dea95e8b6fcc5455e25d90
Joshua M M <joshuamm707@gmail.com>
2025-08-02 17:22:07 +0530

    added apinners

---
+++ src/components/spinners/loadingspinner.js
@@ -0,0 +1,15 @@
+export default function Spinner() {
+
+    return (
+        <div className="spinner-section">
+        <div className="spinner-div">
+            <div className="spinner">
+         
+            </div>
+            <div className="spinner-text">
+        please wait for few seconds ...
+            </div>
+        </div>
+    </div>
+    )
+}
