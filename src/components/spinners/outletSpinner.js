3da8762c9390bc34633801a181d9fbd53e24bd86
Joshua M M <joshuamm707@gmail.com>
2025-08-06 20:22:51 +0530

    added another spinner

---
+++ src/components/spinners/outletSpinner.js
@@ -0,0 +1,15 @@
+export default function Spinner2() {
+
+    return (
+        <div className="outletspinner">
+        <div className="spinner-div">
+            <div className="spinner2">
+         
+            </div>
+            <div className="spinner-text">
+       Loading ...
+            </div>
+        </div>
+    </div>
+    )
+}
